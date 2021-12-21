66f47a8d02808a2d540b786b393a3950e6c3ace1
zhenrongyuan <30374567+zhenrongyuan@users.noreply.github.com>
2021-12-21 00:09:01 -0300

    CuentaController.ts
    Se calcula cuenta asociada a una comanda.

--- controllers/CuentaController.ts
+++ controllers/CuentaController.ts
@@ -1,73 +1,72 @@
-'use strict'
-let ModeloObjeto = require('../models/Comanda');
-let EstadoCuenta = require('../models/EstadoCuenta');
+import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.28.1/mod.ts";
 
-async function obtenerEstado(valor) {
-    let estado = await EstadoCuenta.findOne({ clave: valor }).exec();
-    return estado;
-}
-let estado_cuenta = obtenerEstado('SOL');
-let cuenta = {
+import { db } from "../mongo/client.ts";
+import { Comanda } from "../models/Comanda.ts";
+import { Detalle } from '../models/Detalle.ts';
+import { Estado_cuenta } from '../models/EstadoCuenta.ts';
+import { Cuenta } from '../models/Cuenta.ts';
+import { Medio_de_pago } from '../models/MedioDePago.ts';
+
+const comandas = await db.collection<Comanda>("comandas");
+const estados_cuentas = await db.collection<Estado_cuenta>("estados_cuentas");
+
+const getComandaCalculada = async ({
+    params,
+    response,
+}: {
+    params: { id: string };
+    response: any;
+}) => {
+    const oid = new Bson.ObjectId(params.id);
+    const comanda = await comandas.findOne({
+        _id: oid
+    });
+
+    if (comanda) {
+        let cuentaOutput: Cuenta;
+        if (!comanda.cuenta) {
+            cuentaOutput = cuentaInicial;
+            comanda.cuenta = cuentaOutput;
+        }
+        let comandaOutput = calcularCuenta(comanda, comanda.cuenta);
+        response.status = 200;
+        response.body = {
+            success: true,
+            comanda: comandaOutput,
+            //comanda: comanda
+        };
+    } else {
+        response.status = 404;
+        response.body = {
+            success: false,
+            msg: "Comanda no encontrada.",
+        };
+    }
+};
+
+const estadoCuentaInicial = await estados_cuentas.findOne({ clave: 'SOL' });
+
+const cuentaInicial: Cuenta = {
+    _id: new Bson.ObjectId(),
     subtotal: 0,
     descuento: 0,
     propina: 0,
     total: 0,
-    comentario: '',
-    estado: { estado_cuenta }
+    medio_de_pago: <Medio_de_pago>{},
+    estado: <Estado_cuenta>estadoCuentaInicial,
+    comentario: ''
 };
 
-async function calcularCuenta(request, response) {
-    let modeloObtenidoId = request.params.id;
-
-    ModeloObjeto.findById(modeloObtenidoId).exec(async(error, modeloObtenido) => {
-        if (error) {
-            onServerError(response, error);
-        } else {
-            if (modeloObtenido) {;
-                let comanda_calculada = calcular(modeloObtenido);
-                let estadoSolicitada = await EstadoCuenta.findOne({ clave: 'SOL' }).exec();
-                estadoSolicitada = estadoSolicitada;
-                comanda_calculada.cuenta.estado.clave = estadoSolicitada.clave;
-                comanda_calculada.cuenta.estado.glosa = estadoSolicitada.glosa;
-                return response.status(200).send({
-                    comanda_calculada
-                })
-            } else {
-                onNotFound(response);
-            }
-        }
+function calcularCuenta(comanda: Comanda, cuenta: Cuenta): Comanda {
+    comanda.detalle.forEach(function (detalle: Detalle) {
+        detalle.monto_neto = detalle.producto.precio * detalle.cantidad;
+        cuenta.subtotal = cuenta.subtotal + detalle.monto_neto;
     });
-}
-
-function calcular(comanda) {
-    comanda.cuenta.subtotal = 0;
-    comanda.cuenta.propina = 0;
-    comanda.cuenta.descuento = 1000;
-    comanda.detalle.forEach(function(det) {
-        det.producto.monto_neto = det.producto.precio * det.cantidad;
-
-        comanda.cuenta.subtotal += det.producto.monto_neto;
-    });
-    //comanda.cuenta.total = 1000000 + subtotal;
-    let cuentaConDescuento = comanda.cuenta.subtotal - comanda.cuenta.descuento;
-    comanda.cuenta.propina = cuentaConDescuento * 0.1;
-    comanda.cuenta.total = cuentaConDescuento + comanda.cuenta.propina;
+    cuenta.total = cuenta.subtotal - cuenta.descuento + cuenta.propina;
+    comanda.cuenta = cuenta;
     return comanda;
 }
 
-function onNotFound(response) {
-    response.status(404).send({
-        message: 'No existe comanda.'
-    });
-}
-
-function onServerError(response, error) {
-    response.status(500).send({
-        message: 'Error en el servidor.',
-        error
-    });
-}
-
-module.exports = {
-    calcularCuenta
+export {
+    getComandaCalculada
 };
